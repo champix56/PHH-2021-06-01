68dcefa1d84fa2b4f249f13a15fe1df5cbe1c808
champix <desorbaix@free.fr>
2021-06-03 12:02:13 +0200

    liste by div

--- includes/listeproduit.php
+++ includes/listeproduit.php
@@ -1,1 +1,12 @@
+<style type="text/css">
+    .liste-produits .produit>*{display:inline;vertical-align:middle}
+    .liste-produits .produit img{width:384px;}
+</style>
 <h1>liste produit</h1>
+<div class="liste-produits">
+    <div class="produit">
+        <img src="https://voyagistes.tourismebretagne.com/app/uploads/crt-bretagne-groupes/2020/02/thumbs/une-adt-aa4250-simon-bourcier-morbihan-tourisme-1920x960.jpg" alt=""> <h2 class="titre">titre</h2><h1 class="prix">2.00€</h1>
+        <button type="button" class="btn btn-primary">add cart</button>
+        
+    </div>
+</div>
