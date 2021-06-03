63fa369aab7219d064444ef744753bc6b4345beb
champix <desorbaix@free.fr>
2021-06-03 14:30:08 +0200

    gestion de laffichage liste produits

--- includes/listeproduit.php
+++ includes/listeproduit.php
@@ -1,12 +1,21 @@
-<style type="text/css">
-    .liste-produits .produit>*{display:inline;vertical-align:middle}
-    .liste-produits .produit img{width:384px;}
-</style>
+<?php 
+include_once('sqlfunctions.php');
+$listeprods=getSqlProduits();
+?>
 <h1>liste produit</h1>
-<div class="liste-produits">
-    <div class="produit">
-        <img src="https://voyagistes.tourismebretagne.com/app/uploads/crt-bretagne-groupes/2020/02/thumbs/une-adt-aa4250-simon-bourcier-morbihan-tourisme-1920x960.jpg" alt=""> <h2 class="titre">titre</h2><h1 class="prix">2.00€</h1>
-        <button type="button" class="btn btn-primary">add cart</button>
-        
-    </div>
-</div>
+<table class="liste-produit">
+    <?php
+    for ($i=0; $i < count($listeprods); $i++) { 
+        $unProduitByItterationOfList=$listeprods[$i];
+     ?>
+     <!-- <tr><td colspan="4"><?php print_r($listeprods[$i]);?></td></tr> -->
+    <tr class="produit">
+        <td class="img"><img src="<?= $unProduitByItterationOfList["photo"];?>" alt=""></td>
+        <td class="titre"><?= $unProduitByItterationOfList["titre"];?><br/><?= $unProduitByItterationOfList["prix"];?>€</td>
+        <td class="buttons">
+            <a href="?page=produit&idp=<?= $unProduitByItterationOfList['idpr']?>"><button type="button" class="btn btn-success">voir</button></a><br/>
+             <a href=""><button type="button" class="btn btn-primary">ajouter</button></a>
+        </td>
+    </tr>  
+    <?php } ?>  
+</table>
--- sqlfunctions.php
+++ sqlfunctions.php
@@ -28,12 +28,26 @@ function selectTable($query)
 function getSqlProduit($id)
 {
     // echo 
-    $query='SELECT `idpr`, PR.`titre` AS titre, `prix`, PR.`description` AS decription, `ref`, `photo`, 	PR.`idcat` AS idcat,CA.`titre` AS cat_titre,CA.`description` AS cat_desc FROM `produitS` PR, `categories` CA WHERE CA.`idcat`=PR.`idcat` AND `idpr`='.$id.';';
+    $query='SELECT `idpr`, PR.`titre` AS titre, `prix`, PR.`description` AS decription, `ref`, `photo`, 	PR.`idcat` AS idcat,CA.`titre` AS cat_titre,CA.`description` AS cat_desc FROM `produits` PR, `categories` CA WHERE CA.`idcat`=PR.`idcat` AND `idpr`='.$id.';';
     //reception d'un tableau de resultat possédant q'un seul enregistrement
     //je ne retourne que le 1er (la seule) ligne du tableau 
     $results=selectTable($query);
     //ternaire equivalant a if/else
     //   condition booleen  ?  valeur si cond vrai  :  valeur si cond false
     return $results?$results[0]:false;
+}
+
+
+/**
+ * @param mixed $search search value
+ * 
+ * @return [array[]] 2 dim. array results
+ */
+function getSqlProduits($search=false)
+{
+    // echo 
+    $query='SELECT `idpr`, PR.`titre` AS titre, `prix`, PR.`description` AS decription, `ref`, `photo`, 	PR.`idcat` AS idcat,CA.`titre` AS cat_titre,CA.`description` AS cat_desc FROM `produits` PR, `categories` CA WHERE CA.`idcat`=PR.`idcat`';
+    //retourne tous les reultats de la requette ou false si la requette à echoué ( le parent selectTable gere le retour faux en caxs d'echec de la requette)
+    return selectTable($query);
 }
 ?>
